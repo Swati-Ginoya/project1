8101bfe2b4cd6ed988cfd8385d8fddac8d0d2a63
Swati-Ginoya <5748swatiginoya@gmail.com>
2022-06-01 15:14:22 +0530

    object1

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,79 +1,139 @@
 import logo from './logo.svg';
 import './App.css';
-// import Demo from './component/Demo';
+import Demo from './component/Demo';
 
 function App() {
-  let data =
- [
-    {
-      id: 101,
-      name: 'Abacavir',
-      quantity: 25,
-      price: 150,
-      expiry: 2022,
-      status: true
-    },
-    {
-      id: 102,
-      name: 'Eltrombopag',
-      quantity: 90,
-      price: 550,
-      expiry: 2021,
-      status: true
-    },
-    {
-      id: 103,
-      name: 'Meloxicam',
-      quantity: 85,
-      price: 450,
-      expiry: 2025,
-      status: false
-    },
-    {
-      id: 104,
-      name: 'Allopurinol',
-      quantity: 50,
-      price: 600,
-      expiry: 2023,
-      status: true
-    },
-    {
-      id: 105,
-      name: 'Phenytoin',
-      quantity: 63,
-      price: 250,
-      expiry: 2021,
-      status: false
-    }
-  ];
-  return (
-    <table border = "1" align = "center" cellPadding = "10" cellSpacing="0">
-      <tr>
-        <td>Id</td>
-        <td>Name</td>
-        <td>Quantity</td>
-        <td>Price</td>
-        <td>Expiry</td>
-        <td>Status</td>
-      </tr>
+  // const person = {
+  //   name: "Amit",
+  //   age: 25
+  // }
+  // console.log(person.name,person.age);
 
-      {
-        data.map((d,i) => {
-          let {id,name,quantity,price,expiry,status} =d;
-          return(
-            <tr>
-              <td>{id}</td>
-              <td>{name}</td>
-              <td>{quantity}</td>
-              <td>{price}</td>
-              <td>{expiry}</td>
-              <td>{status.toString()}</td>
-            </tr>
-          )
-        })
+  // return
+
+//   const person = [
+//     {
+//         name: "Amit",
+//         age: 25,
+//     },
+//     {
+//         name: "Piyush",
+//         age: 40,
+//     },
+
+//     ];
+   
+//    person.map((p) => {
+//        console.log(p.name,p.age);
+//    })
+
+// const person = {
+//     name: "Amit",
+//     age: 25,
+//     course: [
+//         "C",
+//         "HTML"
+//     ]
+// }
+ 
+//  for(let k in person){
+//      if(k === 'course'){
+//         person[k].map((p) => console.log(p))
+//      }else{
+//          console.log(person[k]);
+//      }
+//  }
+
+// const person = [
+//     {
+//         name: "Amit",
+//         age: 25,
+//         course: [
+//             "C",
+//             "HTML"
+//         ]
+//     },
+//     {
+//         name: "Ajay",
+//         age: 40,
+//         course: [
+//             "Java",
+//             "JavaScript"
+//         ]
+//     }
+// ]
+// person.map((p) => {
+//     console.log(p.name,p.age);
+//     p.course.map((p1) => console.log(p1))
+// })
+
+
+// const myObj = {
+//     name: "John",
+//     age: 30,
+//     cars: {
+//         car1: "Ford",
+//         car2: "BMW",
+//         car3: "Fiat"
+//     }
+// }
+
+// for(let k in myObj){
+//     if(k === 'cars'){
+//         for(let car in myObj[k]){
+//             console.log(myObj[k][car]);
+//         }
+//     }else{
+//         console.log(myObj[k]);
+//     }
+// }
+
+const data = {
+    personal_info: {
+        name: 'amit',
+        age: 25,
+        city: 'surat'
+    },
+    courses: ["C", "JavaScript", "React"],
+    branches: {
+        rwl: {
+            admission: 50,
+            vacant_seat: 10
+        },
+        rw2: {
+            admission: 30,
+            vacant_seat: 20
+        },
+        rw3: {
+            admission: 25,
+            vacant_seat: 24
+        },
+        rw4: {
+            admission: 40,
+            vacant_seat: 10
+        }
+    }
+}
+let seat = data.branches.rw1;
+  for(let k in data){
+      if(k === 'personal_info'){
+          for(let j in data.personal_info){
+             console.log(data[k][j]);
+          }
       }
-    </table>
-  );
+      if(k === 'courses'){
+          data.courses.map((p) => console.log(p))
+      }
+
+      if(k === 'branches'){
+          for(let j in data.branches){
+              for(let s in data.branches.rwl){
+                  console.log(data[k][j][s]);
+              }
+          }
+      }
+  }console.log(seat);
+ return
 }
 
 export default App;
